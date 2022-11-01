e36111bdbd612f70e29f5af56b4142107cc445af
Nafi'ul Anam <anamnafiul99@gmail.com>
2022-11-01 21:10:54 +0700

    create login page and map page

---
+++ login.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <title>Login</title>
+  <!--
+
+DIGITAL TREND
+
+https://templatemo.com/tm-538-digital-trend
+
+-->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/aos.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+  <!-- MAIN CSS -->
+  <link rel="stylesheet" href="css/templatemo-digital-trend.css">
+
+</head>
+
+<body>
+
+  <!-- FORM -->
+  <section class="contact section-padding">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-lg-6 mx-auto col-md-7 col-12 py-2 mt-2 text-center" data-aos="fade-up">
+          <div class="login100-form-avatar">
+            <img src="images/logo_dinas.png" alt="logo" width="300">
+          </div>
+        </div>
+
+        <div class="col-lg-8 mx-auto col-md-10 col-12">
+
+          <!-- Follow https://templatemo.com/contact page to setup your own contact form -->
+
+          <form action="#" method="post" class="contact-form" data-aos="fade-up" data-aos-delay="300" role="form">
+            <div class="row">
+              <div class="col-lg-12">
+                <input type="text" class="form-control" name="userrname" placeholder="Username">
+              </div>
+            </div>
+            <div class=row>
+              <div class="col-lg-12">
+                <input type="password" class="form-control" name="password" placeholder="Password">
+              </div>
+            </div>
+            <div class="col-lg-5 mx-auto col-7">
+              <button type="submit" class="form-control" id="submit-button" name="submit">Login</button>
+            </div>
+        </div>
+
+        </form>
+      </div>
+
+    </div>
+    </div>
+  </section>
+
+  <!-- SCRIPTS -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/custom.js"></script>
+
+</body>
+
+</html>
