bd8964c39af68740952382b9cc13583b92128130
Nafi'ul Anam <anamnafiul99@gmail.com>
2022-11-09 19:00:09 +0700

    seperate header and body

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<?php
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -33,129 +31,9 @@ https://templatemo.com/tm-538-digital-trend
 
 <body>
 
-     <!-- MENU BAR -->
-     <nav class="navbar navbar-expand-lg">
-          <div class="container">
-               <a class="navbar-brand" href="index.php">
-                    <img src="images/logo1.png" alt="" width="60">
-                    Data SMP Negeri Surabaya
-               </a>
-               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-               </button>
-
-               <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ml-auto">
-                         <li class="nav-item">
-                              <a href="login.php" class="nav-link contact">Login</a>
-                         </li>
-                    </ul>
-               </div>
-          </div>
-     </nav>
-
-
-     <!-- HERO -->
-     <section class="hero hero-bg d-flex justify-content-center align-items-center">
-          <div class="container">
-               <div class="row">
-
-                    <div class="col-lg-6 col-md-10 col-12 d-flex flex-column justify-content-center align-items-center">
-                         <div class="hero-text">
-
-                              <h1 class="text-white" data-aos="fade-up">Selamat Datang di Sistem Informasi Geografis SMP Negeri Kota Surabaya</h1>
-
-                              <a href="map.php" class="custom-btn btn-bg btn mt-3" data-aos="fade-up" data-aos-delay="100">Lihat Peta</a>
-                         </div>
-                    </div>
-
-                    <div class="col-lg-6 col-12">
-                         <div class="hero-image" data-aos="fade-up" data-aos-delay="300">
-
-                              <img src="images/gambar1.png" class="img-fluid" alt="working girl" sha>
-                         </div>
-                    </div>
-
-               </div>
-          </div>
-     </section>
-
-
-     <!-- ABOUT -->
-     <section class="about section-padding pb-0" id="about">
-          <div class="container">
-               <div class="row">
-
-                    <div class="col-lg-7 mx-auto col-md-10 col-12">
-                         <div class="about-info">
-
-                              <h2 class="mb-4" data-aos="fade-up">Tentang Data <strong>SMP Negri Surabaya </strong></h2>
-
-                              <p class="mb-0" data-aos="fade-up">Data SMP Negeri Surabaya Adalah Website Penyedia Informasi Terkait Dengan Seluruh Sekolah Dasar Negri yang Terletak di Kota Surabaya.
-                                   <p>Website ini Menanpilkan Informasi Seperti Alamat, Jumlah Pagu, Visi, Misi, Dll.
-                              </p>
-                         </div>
-
-                         <div class="about-image" data-aos="fade-up" data-aos-delay="200">
-
-                          <div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=2048&amp;height=385&amp;hl=en&amp;q=surabaya&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-                          </iframe><a href="https://proxysite.pro">proxysite</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:385px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:385px;}.gmap_iframe {height:385px!important;}</style></div>
-                         </div>
-                    </div>
-               </div>
-          </div>
-     </section>
-
-
-     <!-- PROJECT -->
-     <!-- TESTIMONIAL -->
-     <section class="testimonial section-padding">
-          <div class="container">
-               
-          </div>
-     </section>
-
-
-     <footer class="site-footer">
-          <div class="container">
-               <div class="row">
-
-                    <div class="col-lg-5 mx-lg-auto col-md-8 col-10">
-                         <h1 class="text-white" data-aos="fade-up" data-aos-delay="100">Sistem Informasi Geografis <strong>SMP N Surabaya</strong></h1>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="200">
-                         <h4 class="my-4">Informasi Kontak</h4>
-
-                         <p class="mb-1">
-                              <i class="fa fa-phone mr-2 footer-icon"></i>
-                              +62 857 7834 9082 
-                         </p>
-
-                         <p>
-                              <a href="#">
-                                   <i class="fa fa-envelope mr-2 footer-icon"></i>
-                                   disdikbud@gmail.com
-                              </a>
-                         </p>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="300">
-                         <h4 class="my-4">Alamat</h4>
-
-                         <p class="mb-1">
-                              <i class="fa fa-home mr-2 footer-icon"></i>
-                              Jalan Jagir Wonokromo No.354-356, Sidosermo, Wonokromo, Sidosermo, Wonocolo, Kota SBY, Jawa Timur 60244, Indonesia.
-                         </p>
-                    </div>
-
-                    <div class="col-lg-4 mx-lg-auto text-center col-md-8 col-12" data-aos="fade-up" data-aos-delay="400">
-                         <p class="copyright-text">Copyright &copy; SIG Kelompok 1 </p>
-                    </div>
-               </div>
-          </div>
-     </footer>
-
+     <?php
+     include('home.php');
+     ?>
 
      <!-- SCRIPTS -->
      <script src="js/jquery.min.js"></script>
