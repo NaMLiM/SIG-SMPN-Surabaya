d968efd560d51fb7a35dcd66f48c263740d4a3dc
Rizal47 <rizalalfianto114@gmail.comm>
2022-11-09 19:53:13 +0700

    map

--- map.php
+++ map.php
@@ -1,1 +1,63 @@
-Peta Here
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <title></title>
+  <!--
+
+DIGITAL TREND
+
+https://templatemo.com/tm-538-digital-trend
+
+-->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/aos.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+  <!-- MAIN CSS -->
+  <link rel="stylesheet" href="css/templatemo-digital-trend.css">
+
+</head>
+
+<body style="background-image: url(images/bg.png); background-size: cover;">
+
+  <!-- FORM -->
+  <nav class="navbar navbar-expand-lg">
+          <div class="container">
+               <a class="navbar-brand" href="index.php">
+                    <img src="images/logo1.png" alt="" width="60">
+                    MAP
+               </a>
+               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+               </button>
+          </div>
+     </nav>
+
+     <!-- CONTENT -->
+     <section class="justify-content-center align-items-center">
+          <div class="container">
+               
+          </div>
+     </section>
+
+  <!-- SCRIPTS -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/custom.js"></script>
+
+</body>
+
+</html>
